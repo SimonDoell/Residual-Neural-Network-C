fda14647ce91a5a9f3e5a5b1230d3e5806f135e2
Simon Döll <doell.simonf@gmail.com>
2026-09-07 07:49:41 +0200

    Working example

--- src/activation_layer.h
+++ src/activation_layer.h
@@ -12,6 +12,14 @@ static inline float ReLU_derivative(float x) {return (x > 0.0f ? 1 : 0.0f);}
 static inline float Tanh_forward   (float x) {return tanhf(x);}
 static inline float Tanh_derivative(float x) {return 1.0f - powf(tanhf(x), 2);}
 
+static const float leakage = 1.15f;
+static inline float leakyTanh_forward   (float x) {return tanhf(x) * leakage;}
+static inline float leakyTanh_derivative(float x) {return (1.0f - powf(tanhf(x), 2)) * leakage;}
+
+static const float liniear_factor = 0.1f;
+static inline float linearTanh_forward   (float x) {return tanhf(x) + liniear_factor * x;}
+static inline float linearTanh_derivative(float x) {return (1.0f - powf(tanhf(x), 2)) + liniear_factor;}
+
 typedef struct {
     float(*forward)(float);
     float(*derivative)(float);
@@ -53,6 +61,8 @@ static inline Layer activation_layer_new(float(*forward)(float), float(*derivati
 
 static inline Layer ReLU_new() {return activation_layer_new(ReLU_forward, ReLU_derivative);}
 static inline Layer Tanh_new() {return activation_layer_new(Tanh_forward, Tanh_derivative);}
+static inline Layer leakyTanh_new() {return activation_layer_new(leakyTanh_forward, leakyTanh_derivative);}
+static inline Layer linearTanh_new() {return activation_layer_new(linearTanh_forward, linearTanh_derivative);}
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -7,53 +7,67 @@
 
 #define countof(arr) (sizeof(arr) / sizeof(arr[0]))
 
+float learn_function(float x) {
+    return sin(x * 3.14159265358979f * 2.0f);
+}
+
 int main() {srand(time(0));
     NeuralNetwork network = nn_init();
     network.learning_rate = 0.001f;
-    uint32_t l1, l2, l3, l4, l5, l6;
+    float scaling = 0.05f;
+
+    nn_add_layer(&network, linear_adam_new_default(1, 16, scaling), true);
+    nn_add_layer(&network, ReLU_new(), true);
     
-    l1 = nn_add_layer(&network, linear_adam_new_default(1 , 16, 0.2f), true);
-         nn_add_layer(&network, ReLU_new(), true);
-    l2 = nn_add_layer(&network, linear_adam_new_default(16, 16, 0.2f), true);
-         nn_add_layer(&network, ReLU_new(), true);
-    l3 = nn_add_layer(&network, linear_adam_new_default(16, 16, 0.2f), true);
-         nn_add_layer(&network, ReLU_new(), true);
-    l4 = nn_add_layer(&network, linear_adam_new_default(16, 16, 0.2f), true);
-         nn_add_layer(&network, ReLU_new(), true);
-    l5 = nn_add_layer(&network, linear_adam_new_default(16, 16, 0.2f), true);
-         nn_add_layer(&network, ReLU_new(), true);
-    l6 = nn_add_layer(&network, linear_adam_new_default(16, 1 , 0.2f), true);
-         nn_add_layer(&network, Tanh_new(), true);
-        
-    nn_connect_layers(&network, l1, l2);
-    nn_connect_layers(&network, l2, l3);
-    nn_connect_layers(&network, l3, l4);
-    nn_connect_layers(&network, l4, l5);
-    nn_connect_layers(&network, l5, l6);
-    
-    nn_connect_layers(&network, l1, l6);
-    
+    for (int i = 0; i < 40; ++i) {
+        nn_add_layer(&network, linear_adam_new_default(16, 16, scaling), true);
+        nn_add_layer(&network, ReLU_new(), true);
+    }
+
+    nn_add_layer(&network, linear_adam_new_default(16, 1, scaling), true);
+    nn_add_layer(&network, leakyTanh_new(), true);
+
+    for (int i = 1; i < network.layer_count-4; i+=2) {
+        nn_connect_layers(&network, i, i+2);
+    }
 
     nn_order_layers(&network); // Finalizing the NN structure
 
-    printf("Ordering: ");
-    for (int i = 0; i < network.layer_count; ++i)
-        printf("%i ", network.graph_order[i]);
-    printf("\n");
+    // printf("Ordering: ");
+    // for (int i = 0; i < network.layer_count; ++i)
+    //     printf("%i ", network.graph_order[i]);
+    // printf("\n");
 
-    Matrix input = mat_vector(1);
-    input.values[0] = 1.0f;
-
+    Matrix input  = mat_vector(1);
     Matrix output = mat_vector(1);
-    output.values[0] = 0.5706f;
 
-    for (int i = 0; i < 10000; ++i)
+    // --- TRAINING ---
+    uint32_t train_iter = 100000;
+    for (int i = 0; i < train_iter; ++i) {
+        input .values[0] = rand_float(0, 1);
+        output.values[0] = learn_function(input.values[0]);
         nn_train(&network, &input, &output);
 
-    mat_free(&output);
-    output = nn_forward(&network, &input);
+        if (i % (train_iter / 10) == 0 && i != 0) {
+            printf("Percentage: %i Percent\n", i / (train_iter / 100));
+        }
+    }
 
-    printf("Output %ix%i: %f\n", output.rows, output.cols, output.values[0]);
+    // --- TESTING ---
+    float error = 0.0f;
+    uint32_t iter = 100000;
+    
+    for (int i = 0; i < iter; ++i) {
+        input.values[0] = rand_float(0, 1);
+
+        mat_free(&output);
+        output = nn_forward(&network, &input);
+
+        error += fabsf(learn_function(input.values[0]) - output.values[0]);
+    }
+
+    error = error / (float)iter;
+    printf("Error (amortized): %f\n", error);
 
     mat_free(&input);
     mat_free(&output);
